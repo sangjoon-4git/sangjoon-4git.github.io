278b291c7d93302b953770d5cbbce8c1d934145b
sangjoon-4git <dutkdwns59@gmail.com>
2025-09-25 13:45:01 +0900

    update hw2

--- HW2/hw2vert.glsl
+++ HW2/hw2vert.glsl
@@ -1,11 +1,11 @@
 #version 300 es
 layout (location = 0) in vec3 aPos;
 
-uniform vec3 movement;
+uniform vec2 movement;
 
 void main() {
     gl_Position = vec4(aPos[0] + movement.x, 
                        aPos[1] + movement.y,
-                       aPos[2] + movement.z,
+                       aPos[2],
                        1.0);
 }
